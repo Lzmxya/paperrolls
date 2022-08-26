e71a3ad1431ddd6ba57b1310d2300e7dfc906a62
Lzmxya <lzmxya@gmail.com>
2022-08-26 19:51:28 +0800

    feat: add a droppable area for import feature

---
+++ src/components/receipts/ReceiptsEmpty.tsx
@@ -0,0 +1,13 @@
+function ReceiptsEmpty() {
+  return (
+    <div className="flex grow flex-col self-center text-center">
+      <h2 className="m-2 text-xl">沒有發票</h2>
+      <p className="text-gray-500">
+        將由財政部寄送的「消費資訊」郵件中的 .csv
+        附件拖曳至此，或輕觸左側選單的「＋新增發票」按鈕以匯入發票。
+      </p>
+    </div>
+  );
+}
+
+export default ReceiptsEmpty;
--- src/pages/Receipts.tsx
+++ src/pages/Receipts.tsx
@@ -1,6 +1,41 @@
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+import { FileWithPath } from "file-selector";
+import ReceiptsEmpty from "../components/receipts/ReceiptsEmpty";
+
 function Receipts() {
+  const onDrop = useCallback((acceptedFiles: FileWithPath[]) => {
+    console.log(acceptedFiles);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    accept: {
+      "text/csv": [".csv"],
+    },
+    maxFiles: 1,
+    noClick: true,
+    noKeyboard: true,
+    onDrop,
+  });
+
   return (
-    <main className="ml-[4.5rem] mb-2 mr-2 w-full rounded-xl bg-white p-8"></main>
+    <main
+      {...getRootProps()}
+      className={
+        "ml-[4.5rem] mb-2 mr-2 flex w-full rounded-xl bg-white " +
+        (isDragActive && "border-2 border-blue-400")
+      }
+    >
+      <input {...getInputProps()} />
+      {isDragActive ? (
+        <div className="grow self-center text-center">
+          <p className="text-gray-500">
+            將由財政部寄送的「消費資訊」郵件中的 .csv 附件拖曳至此即可匯入發票。
+          </p>
+        </div>
+      ) : (
+        <ReceiptsEmpty />
+      )}
+    </main>
   );
 }
 
