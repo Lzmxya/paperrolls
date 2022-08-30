98e652ce106945ace6121499a6027d02c5efb2e4
Lzmxya <lzmxya@gmail.com>
2022-08-30 22:15:25 +0800

    feat: create models for database

---
+++ src/models/Receipt.ts
@@ -0,0 +1,41 @@
+import { formatDate } from "../utils";
+
+export interface Receipt {
+  // id?: number;
+  cardType: string;
+  cardNo: string;
+  invDate: Date;
+  sellerBan: number;
+  sellerName: string;
+  invNum: string;
+  amount: number;
+  invStatus: string;
+  details: { amount: number; description: string }[];
+}
+
+export class Receipt {
+  constructor(
+    cardType: string,
+    cardNo: string,
+    invDate: string,
+    sellerBan: number,
+    sellerName: string,
+    invNum: string,
+    amount: number,
+    invStatus: string
+  ) {
+    this.cardType = cardType;
+    this.cardNo = cardNo;
+    this.invDate = new Date(formatDate(invDate, "####-##-##", "GMT+08:00"));
+    this.sellerBan = +sellerBan;
+    this.sellerName = sellerName;
+    this.invNum = invNum;
+    this.amount = +amount;
+    this.invStatus = invStatus;
+    this.details = [];
+  }
+
+  addDetail(amount: number, description: string) {
+    this.details.push({ amount, description });
+  }
+}
---
+++ src/models/db.ts
@@ -0,0 +1,21 @@
+import Dexie, { Table } from "dexie";
+
+import { Receipt } from "./Receipt";
+
+export class ReceiptsDB extends Dexie {
+  receipts!: Table<Receipt, number>;
+  constructor() {
+    super("ReceiptsDB");
+    this.version(1).stores({
+      receipts: "invNum",
+    });
+  }
+}
+
+export const db = new ReceiptsDB();
+
+export function resetDatabase() {
+  return db.transaction("rw", db.receipts, async () => {
+    await Promise.all(db.tables.map((table) => table.clear()));
+  });
+}
