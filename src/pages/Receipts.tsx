6d2150c21d6deb9c62f099a7546fface8fa5a712
Lzmxya <lzmxya@gmail.com>
2022-09-05 14:50:50 +0800

    perf: prevent re-rendering after isDragActive

--- src/pages/Receipts.tsx
+++ src/pages/Receipts.tsx
@@ -9,6 +9,7 @@ function Receipts() {
   const onDrop = useCallback((acceptedFiles: FileWithPath[]) => {
     importReceipts(acceptedFiles);
   }, []);
+
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     accept: {
       "text/csv": [".csv"],
@@ -21,21 +22,20 @@ function Receipts() {
   return (
     <main
       {...getRootProps()}
-      className={
-        "ml-[4.5rem] mb-2 mr-2 flex w-full overflow-hidden rounded-xl bg-white " +
-        (isDragActive && "border-2 border-blue-400")
-      }
+      className="relative ml-[4.5rem] mb-2 mr-2 flex w-full overflow-hidden rounded-xl bg-white "
     >
       <input {...getInputProps()} />
-      {isDragActive ? (
-        <div className="grow self-center text-center">
-          <p className="text-gray-500">
-            將由財政部寄送的「消費資訊」郵件中的 .csv 附件拖曳至此即可匯入發票。
-          </p>
+      {isDragActive && (
+        <div className="absolute z-10 flex h-full w-full items-center justify-center rounded-xl border-2 border-blue-400 bg-blue-100">
+          <div>
+            <p className="text-gray-500">
+              將由財政部寄送的「消費資訊」郵件中的 .csv
+              附件拖曳至此即可匯入發票。
+            </p>
+          </div>
         </div>
-      ) : (
-        <ReceiptsInbox />
       )}
+      <ReceiptsInbox />
     </main>
   );
 }
