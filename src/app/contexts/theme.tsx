3d961cacdeb9dcfbbf1e8c22dedc8f5aa9910f31
Lzmxya <lzmxya@gmail.com>
2022-10-30 23:51:49 +0800

    feat: prepare for manually toggling dark mode

---
+++ src/app/contexts/theme.tsx
@@ -0,0 +1,47 @@
+import {
+  Dispatch,
+  ReactElement,
+  SetStateAction,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { useLocalStorage, useMedia } from "react-use";
+
+type ThemeName = "dark" | "light" | "system";
+
+type ThemeContextData = {
+  theme: ThemeName;
+  setTheme: Dispatch<SetStateAction<ThemeName>>;
+};
+
+interface ThemeContextProps {
+  children: ReactElement;
+}
+
+const ThemeContext = createContext({} as ThemeContextData);
+
+export function ThemeProvider({ children }: ThemeContextProps) {
+  const isPrefersDark = useMedia("(prefers-color-scheme: dark)");
+  const [themeConfig, setThemeConfig] = useLocalStorage<ThemeName>("theme");
+  const [theme, setTheme] = useState(themeConfig || "system");
+
+  useEffect(() => {
+    if (theme === "dark" || (theme === "system" && isPrefersDark)) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+
+    setThemeConfig(theme);
+  }, [isPrefersDark, theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export const useTheme = () => useContext(ThemeContext);
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+import { ThemeProvider } from "./app/contexts/theme";
 import { RouterProvider } from "react-router-dom";
 import router from "./app/router";
 import "./index.css";
@@ -9,7 +10,9 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <ThemeProvider>
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
