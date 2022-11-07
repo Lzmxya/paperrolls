002e880814561852946e8bc8d88dafff20265254
Lzmxya <lzmxya@gmail.com>
2022-11-07 22:27:45 +0800

    feat: add dark theme for route error page

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -6,13 +6,13 @@ export default function ErrorBoundary() {
 
   if (isRouteErrorResponse(error)) {
     return (
-      <div className="p-4">
+      <div className="h-screen p-4 dark:bg-black dark:text-white">
         <h1 className="text-xl">錯誤</h1>
         <h2>
           {error.status}：{error.statusText}
         </h2>
         {error.data?.message && <p>{error.data.message}</p>}
-        <Link to="/" className="text-blue-600">
+        <Link to="/" className="text-blue-600 dark:text-blue-400">
           前往首頁
         </Link>
       </div>
