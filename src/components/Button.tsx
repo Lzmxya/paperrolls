4d7966048d10be105894888ed36f8bb0919bac0a
Lzmxya <lzmxya@gmail.com>
2022-11-17 18:40:07 +0800

    feat: adjust Button style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -16,12 +16,12 @@ export default function Button({
   return (
     <button
       type="button"
-      className={`h-10 rounded-full px-3 transition-all ${
-        filled && "bg-blue-200 dark:bg-blue-400/50"
-      } ${
+      className={`h-10 rounded-full px-3 text-blue-600 transition-all dark:text-blue-300 ${
         disabled
           ? "opacity-40"
-          : "hover:bg-blue-300 active:bg-blue-400 dark:hover:bg-blue-300/50 dark:active:bg-blue-400"
+          : filled
+          ? "bg-blue-200 text-inherit hover:bg-blue-300 active:bg-blue-400 dark:bg-blue-400/50 dark:text-inherit dark:hover:bg-blue-300/50 dark:active:bg-blue-200/50"
+          : "hover:bg-blue-300/40 active:bg-blue-300/60 dark:hover:bg-blue-300/20 dark:active:bg-blue-300/40"
       }`}
       onClick={onClick}
       disabled={disabled || false}
