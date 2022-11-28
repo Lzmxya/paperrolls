317a3bd52dc3f88c8d74e0c87240f34b258ca112
Lzmxya <lzmxya@gmail.com>
2022-11-28 23:50:31 +0800

    refactor: clean up stuff related to Fuse.js

--- src/features/search/fuse-options.ts
+++
@@ -1,3 +0,0 @@
-export const fuseOptions = {
-  keys: ["sellerName", "details.description"],
-};
--- src/features/search/index.ts
+++ src/features/search/index.ts
@@ -1,5 +1,3 @@
 export * from "./components/SearchField";
 export * from "./components/SearchHighlighter";
 export * from "./hooks/useSearchHotkeys";
-export * from "./utils/query-builder";
-export * from "./fuse-options";
