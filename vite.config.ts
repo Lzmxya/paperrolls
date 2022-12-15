89077e387a87f127109d779ba352b3e7a7f0a4fd
Lzmxya <lzmxya@gmail.com>
2022-12-16 00:55:44 +0800

    build: install, set up `rollup-plugin-visualizer`

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig, type PluginOption } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import { visualizer } from "rollup-plugin-visualizer";
 
 import { fileURLToPath, URL } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), visualizer() as PluginOption],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
