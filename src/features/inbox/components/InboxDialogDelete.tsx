305125847f4321b667ea5392d299db4216c05784
Lzmxya <lzmxya@gmail.com>
2023-02-11 16:56:12 +0800

    fix: update receiptGroups after deleting receipts

--- src/features/inbox/components/InboxDialogDelete.tsx
+++ src/features/inbox/components/InboxDialogDelete.tsx
@@ -1,30 +1,46 @@
 import { useEffect, useState } from "react";
+import { format } from "date-fns";
 
 import { useAppDispatch, useAppSelector } from "@/app/hooks";
 import { clearChecked, clearDeleting, toggleChecked } from "../inboxSlice";
 
-import { db } from "@/models";
+import { db, ReceiptGroup } from "@/models";
 
 import Button from "@/components/Button";
 import Dialog from "@/components/Dialog";
 
 export function InboxDialogDelete() {
   const dispatch = useAppDispatch();
-  const { checkedReceipts, deletingReceipts } = useAppSelector(
-    (state) => state.inbox
-  );
+  const { checkedReceipts, deletingReceipts: deletingReceiptIds } =
+    useAppSelector((state) => state.inbox);
   const [dialogIsOpen, setDialogIsOpen] = useState(false);
   const [dialogHeadline, setDialogHeadline] = useState("");
   const handleDelete = () => {
     setDialogIsOpen(false);
-    db.receipts.bulkDelete(deletingReceipts);
-    if (deletingReceipts.length > 1) {
+    db.transaction("rw", db.receipts, db.receiptGroups, async () => {
+      const deletingReceipts = await db.receipts.bulkGet(deletingReceiptIds);
+      deletingReceipts.forEach((deletingReceipt) => {
+        if (!deletingReceipt) return;
+        db.receiptGroups
+          .where({ month: format(deletingReceipt.invDate, "yyyy-MM") })
+          .modify((receiptGroup, ref: { value?: ReceiptGroup }) => {
+            if (receiptGroup.counts > 1) {
+              receiptGroup.counts -= 1;
+              receiptGroup.total -= deletingReceipt.amount;
+              return;
+            }
+            delete ref.value;
+          });
+      });
+      db.receipts.bulkDelete(deletingReceiptIds);
+    });
+    if (deletingReceiptIds.length > 1) {
       dispatch(clearChecked());
       dispatch(clearDeleting());
       return;
     }
-    if (checkedReceipts.includes(deletingReceipts[0])) {
-      dispatch(toggleChecked(deletingReceipts[0]));
+    if (checkedReceipts.includes(deletingReceiptIds[0])) {
+      dispatch(toggleChecked(deletingReceiptIds[0]));
       dispatch(clearDeleting());
       return;
     }
@@ -32,14 +48,14 @@ export function InboxDialogDelete() {
   };
 
   useEffect(() => {
-    if (deletingReceipts.length === 0) return;
-    if (deletingReceipts.length === 1) {
-      setDialogHeadline(`刪除「${deletingReceipts[0]}」？`);
+    if (deletingReceiptIds.length === 0) return;
+    if (deletingReceiptIds.length === 1) {
+      setDialogHeadline(`刪除「${deletingReceiptIds[0]}」？`);
     } else {
       setDialogHeadline("刪除多張發票？");
     }
     setDialogIsOpen(true);
-  }, [deletingReceipts]);
+  }, [deletingReceiptIds]);
 
   return (
     <Dialog
