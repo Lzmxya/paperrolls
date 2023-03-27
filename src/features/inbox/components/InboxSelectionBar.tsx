6eec27f70f3c42a31f96f18a337ab0336f91968a
Lzmxya <lzmxya@gmail.com>
2023-03-27 22:35:19 +0800

    feat: add ability to archive multiple receipts
    Based on commit d02ff87.

--- src/features/inbox/components/InboxSelectionBar.tsx
+++ src/features/inbox/components/InboxSelectionBar.tsx
@@ -1,20 +1,35 @@
+import { useLiveQuery } from "dexie-react-hooks";
 import { useAppDispatch, useAppSelector } from "@/app/hooks";
 import { clearChecked, setDeleting } from "../inboxSlice";
+import { archiveReceipts, db } from "@/models";
 
 import DropdownMenu from "@/components/DropdownMenu";
 import IconButton from "@/components/IconButton";
+import { ReactComponent as IconArchive } from "@/assets/images/icons/archive.svg";
 import { ReactComponent as IconClose } from "@/assets/images/icons/close.svg";
 import { ReactComponent as IconDelete } from "@/assets/images/icons/delete.svg";
+import { ReactComponent as IconStar } from "@/assets/images/icons/star.svg";
+import { ReactComponent as IconUnarchive } from "@/assets/images/icons/unarchive.svg";
 
 export function InboxSelectionBar() {
   const dispatch = useAppDispatch();
   const { checkedReceipts } = useAppSelector((state) => state.inbox);
+  const { isAllArchived, isAllStarred } = useLiveQuery(
+    async () => {
+      const receipts = await db.receipts.bulkGet(checkedReceipts);
+      const isAllArchived = receipts.every((receipt) => receipt?.archived);
+      const isAllStarred = receipts.every((receipt) => receipt?.starred);
+      return { isAllArchived, isAllStarred };
+    },
+    [checkedReceipts],
+    { isAllArchived: false, isAllStarred: false }
+  );
 
   const menuItems = [
     {
-      label: "刪除",
-      icon: <IconDelete />,
-      onClick: () => dispatch(setDeleting(checkedReceipts)),
+      label: isAllStarred ? "移除星號" : "加上星號",
+      icon: <IconStar />,
+      onClick: () => null,
     },
   ];
 
@@ -28,7 +43,19 @@ export function InboxSelectionBar() {
         />
         <span className="text-xl">已選取 {checkedReceipts.length} 張發票</span>
       </div>
-      <DropdownMenu items={menuItems} icons />
+      <div className="flex">
+        <IconButton
+          label={isAllArchived ? "取消封存" : "封存"}
+          icon={isAllArchived ? <IconUnarchive /> : <IconArchive />}
+          onClick={() => archiveReceipts(checkedReceipts)}
+        />
+        <IconButton
+          label="刪除"
+          icon={<IconDelete />}
+          onClick={() => dispatch(setDeleting(checkedReceipts))}
+        />
+        <DropdownMenu items={menuItems} icons />
+      </div>
     </div>
   );
 }
