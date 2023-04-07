95bf36521e30cbbb4e052f54db278e8cc9ec1ee6
Lzmxya <lzmxya@gmail.com>
2023-04-08 01:07:01 +0800

    feat: add message about errors caused by private…
    …browsing mode

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -6,7 +6,7 @@ export default function ErrorBoundary() {
 
   if (isRouteErrorResponse(error)) {
     return (
-      <div className="h-screen p-4 dark:bg-black dark:text-white">
+      <div className="h-screen space-y-2 p-4 dark:bg-black dark:text-white">
         <h1 className="text-xl">錯誤</h1>
         <h2>
           {error.status}：{error.statusText}
@@ -20,8 +20,16 @@ export default function ErrorBoundary() {
   }
 
   return (
-    <div>
-      <p>錯誤</p>
+    <div className="space-y-2 p-4">
+      <h1 className="text-xl">錯誤</h1>
+      <ul className="list-disc space-y-2">
+        <li>
+          已知本應用程式無法在部分瀏覽器的「私密瀏覽」模式下正確執行，請將之停用後再試一次。
+        </li>
+        <li>
+          若出現本畫面時並未啟用私密瀏覽模式，請至本專案的問題追蹤器回報錯誤。
+        </li>
+      </ul>
     </div>
   );
 }
