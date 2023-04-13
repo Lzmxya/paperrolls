ffb060c57d93d1e5ad9f04df0575286efe0c5de8
Lzmxya <lzmxya@gmail.com>
2023-04-13 17:50:28 +0800

    feat: add issues tracker link to ErrorBoundary

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -12,7 +12,10 @@ export default function ErrorBoundary() {
           {error.status}：{error.statusText}
         </h2>
         {error.data?.message && <p>{error.data.message}</p>}
-        <Link to="/" className="text-blue-600 dark:text-blue-400">
+        <Link
+          to="/"
+          className="text-blue-600 hover:underline dark:text-blue-400"
+        >
           前往首頁
         </Link>
       </div>
@@ -27,7 +30,16 @@ export default function ErrorBoundary() {
           已知本應用程式無法在部分瀏覽器的「私密瀏覽」模式下正確執行，請將之停用後再試一次。
         </li>
         <li>
-          若出現本畫面時並未啟用私密瀏覽模式，請至本專案的問題追蹤器回報錯誤。
+          若出現本畫面時並未啟用私密瀏覽模式，請至本專案的
+          <a
+            href="https://github.com/Lzmxya/paperrolls/issues"
+            target="_blank"
+            rel="noreferrer"
+            className="text-blue-600 hover:underline dark:text-blue-400"
+          >
+            問題追蹤器
+          </a>
+          回報錯誤。
         </li>
       </ul>
     </div>
