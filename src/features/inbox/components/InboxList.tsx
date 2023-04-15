dc08435eb53fe167808b0326fc2e58211e50f945
Lzmxya <lzmxya@gmail.com>
2023-04-15 11:16:48 +0800

    fix: avoid `scrollIntoView` overflow list item

--- src/features/inbox/components/InboxList.tsx
+++ src/features/inbox/components/InboxList.tsx
@@ -79,119 +79,121 @@ export function InboxList({ receipts, receiptGroups }: InboxListProps) {
           } = receipts[index];
 
           return (
-            <div
-              onClick={() =>
-                dispatch(
-                  setSelected({
-                    previous: index - 1,
-                    current: index,
-                    next: index + 1,
-                  })
-                )
-              }
-              className={`group relative mx-2 my-1 flex cursor-pointer rounded-2xl transition-all dark:border-neutral-700 dark:hover:text-white md:mx-1 ${
-                (selectedReceipt.current === index ||
-                  checkedReceipts.includes(invNum)) &&
-                "dark:text-white"
-              } ${
-                checkedReceipts.includes(invNum)
-                  ? "bg-blue-200 hover:bg-blue-300 dark:bg-blue-400/50 dark:hover:bg-blue-400/60"
-                  : selectedReceipt.current === index
-                  ? "bg-blue-100 hover:bg-blue-200 dark:bg-blue-200/20 dark:hover:bg-blue-400/30"
-                  : archived
-                  ? "bg-black/5 hover:bg-blue-50 dark:bg-black"
-                  : "hover:bg-blue-50 dark:hover:bg-neutral-800"
-              }`}
-            >
-              {/* Indicator */}
-              {selectedReceipt.current === index && (
-                <div className="absolute flex h-full w-4 items-center justify-center">
-                  <div className="h-6 w-1 rounded-full bg-blue-400"></div>
-                </div>
-              )}
-              {/* Supporting visuals */}
+            <div className="px-2 py-0.5 md:px-1">
               <div
-                className="h-20 w-20 shrink-0 p-4"
-                onClick={(event) => {
-                  event.stopPropagation();
-                  dispatch(toggleChecked(invNum));
-                }}
+                onClick={() =>
+                  dispatch(
+                    setSelected({
+                      previous: index - 1,
+                      current: index,
+                      next: index + 1,
+                    })
+                  )
+                }
+                className={`group relative flex cursor-pointer rounded-2xl transition-all dark:border-neutral-700 dark:hover:text-white ${
+                  (selectedReceipt.current === index ||
+                    checkedReceipts.includes(invNum)) &&
+                  "dark:text-white"
+                } ${
+                  checkedReceipts.includes(invNum)
+                    ? "bg-blue-200 hover:bg-blue-300 dark:bg-blue-400/50 dark:hover:bg-blue-400/60"
+                    : selectedReceipt.current === index
+                    ? "bg-blue-100 hover:bg-blue-200 dark:bg-blue-200/20 dark:hover:bg-blue-400/30"
+                    : archived
+                    ? "bg-black/5 hover:bg-blue-50 dark:bg-black"
+                    : "hover:bg-blue-50 dark:hover:bg-neutral-800"
+                }`}
               >
-                {checkedReceipts.includes(invNum) ? (
-                  <div className="flex h-12 w-12 items-center justify-center rounded-full bg-blue-400">
-                    <Check className="fill-current" />
+                {/* Indicator */}
+                {selectedReceipt.current === index && (
+                  <div className="absolute flex h-full w-4 items-center justify-center">
+                    <div className="h-6 w-1 rounded-full bg-blue-400"></div>
                   </div>
-                ) : (
-                  <Avatar name={sellerName} />
                 )}
-              </div>
-              {/* Primary text */}
-              <div className="m-auto grow overflow-hidden text-sm">
-                <p
-                  className={`truncate font-bold after:ml-2 ${
-                    archived &&
-                    "after:rounded after:bg-black/50 after:px-1 after:py-0.5 after:text-xs after:font-normal after:text-white after:content-['封存'] dark:after:bg-white dark:after:text-black"
-                  }`}
+                {/* Supporting visuals */}
+                <div
+                  className="h-20 w-20 shrink-0 p-4"
+                  onClick={(event) => {
+                    event.stopPropagation();
+                    dispatch(toggleChecked(invNum));
+                  }}
                 >
-                  {invNum}
-                </p>
-                <p className="truncate opacity-80">
-                  <SearchHighlighter content={sellerName} />
-                </p>
-                <p className="truncate opacity-80">
-                  <SearchHighlighter
-                    content={receiptDetailPreviewString(details)}
-                  />
-                </p>
-              </div>
-              {/* Metadata */}
-              {/* TODO: FormatJS */}
-              <div className="flex flex-col items-end justify-between whitespace-nowrap pl-2">
-                <div className="mr-3 mt-[0.625rem]">
-                  <p className="text-sm">{amount} 元</p>
+                  {checkedReceipts.includes(invNum) ? (
+                    <div className="flex h-12 w-12 items-center justify-center rounded-full bg-blue-400">
+                      <Check className="fill-current" />
+                    </div>
+                  ) : (
+                    <Avatar name={sellerName} />
+                  )}
                 </div>
-                <div className="flex items-end">
-                  <span className="mb-[0.625rem] text-xs  group-hover:invisible">
-                    {format(
-                      invDate,
-                      isThisYear(invDate)
-                        ? isThisWeek(invDate)
-                          ? "iii"
-                          : "MMMdo"
-                        : "yyyy/M/d",
-                      {
-                        locale: zhTW,
-                      }
-                    )}
-                  </span>
-                  <ul
-                    className={`flex h-10 group-hover:opacity-100 ${
-                      !starred && "opacity-60"
+                {/* Primary text */}
+                <div className="m-auto grow overflow-hidden text-sm">
+                  <p
+                    className={`truncate font-bold after:ml-2 ${
+                      archived &&
+                      "after:rounded after:bg-black/50 after:px-1 after:py-0.5 after:text-xs after:font-normal after:text-white after:content-['封存'] dark:after:bg-white dark:after:text-black"
                     }`}
                   >
-                    <li className="hidden group-hover:list-item">
-                      <IconButton
-                        label="刪除"
-                        icon={<Delete />}
-                        onClick={() => dispatch(setDeleting([invNum]))}
-                      />
-                    </li>
-                    <li className="hidden group-hover:list-item">
-                      <IconButton
-                        label={archived ? "取消封存" : "封存"}
-                        icon={archived ? <Unarchive /> : <Archive />}
-                        onClick={() => {
-                          archiveReceipts([invNum]);
-                          dispatch(
-                            archived ? resetToast() : setArchivedToast(invNum)
-                          );
-                        }}
-                      />
-                    </li>
-                    <li>
-                      <InboxButtonStar invNum={invNum} starred={starred} />
-                    </li>
-                  </ul>
+                    {invNum}
+                  </p>
+                  <p className="truncate opacity-80">
+                    <SearchHighlighter content={sellerName} />
+                  </p>
+                  <p className="truncate opacity-80">
+                    <SearchHighlighter
+                      content={receiptDetailPreviewString(details)}
+                    />
+                  </p>
+                </div>
+                {/* Metadata */}
+                {/* TODO: FormatJS */}
+                <div className="flex flex-col items-end justify-between whitespace-nowrap pl-2">
+                  <div className="mr-3 mt-[0.625rem]">
+                    <p className="text-sm">{amount} 元</p>
+                  </div>
+                  <div className="flex items-end">
+                    <span className="mb-[0.625rem] text-xs  group-hover:invisible">
+                      {format(
+                        invDate,
+                        isThisYear(invDate)
+                          ? isThisWeek(invDate)
+                            ? "iii"
+                            : "MMMdo"
+                          : "yyyy/M/d",
+                        {
+                          locale: zhTW,
+                        }
+                      )}
+                    </span>
+                    <ul
+                      className={`flex h-10 group-hover:opacity-100 ${
+                        !starred && "opacity-60"
+                      }`}
+                    >
+                      <li className="hidden group-hover:list-item">
+                        <IconButton
+                          label="刪除"
+                          icon={<Delete />}
+                          onClick={() => dispatch(setDeleting([invNum]))}
+                        />
+                      </li>
+                      <li className="hidden group-hover:list-item">
+                        <IconButton
+                          label={archived ? "取消封存" : "封存"}
+                          icon={archived ? <Unarchive /> : <Archive />}
+                          onClick={() => {
+                            archiveReceipts([invNum]);
+                            dispatch(
+                              archived ? resetToast() : setArchivedToast(invNum)
+                            );
+                          }}
+                        />
+                      </li>
+                      <li>
+                        <InboxButtonStar invNum={invNum} starred={starred} />
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
